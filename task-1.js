b775480fd77ea527d47bef6a6c37cba331da256d
Nicola <menekse776@gmail.com>
2025-01-23 06:43:17 +0300

    secondcommit

--- task-1.js
+++
@@ -1,9 +0,0 @@
-function slugify(title) {
-    return title.split(" ").join("-").toLowerCase()
-}
-
-
-slugify("Arrays for beginners")
-slugify("English for developer")
-slugify("Ten secrets of JavaScript")
-slugify("How to become a JUNIOR developer in TWO WEEKS")
